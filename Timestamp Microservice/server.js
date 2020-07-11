4b6cf1b08175d89f7175f56a389e3ae2a972ecb6
Mridul <mridulmahajan0001@gmail.com>
2020-07-11 11:00:39 +0530

    Freecodecamp - APIs and Microservices Timestamp Microservice

---
+++ Timestamp Microservice/server.js
@@ -0,0 +1,53 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.get("/api/timestamp/:date_string?", function(req, res){
+  
+  var date = null;
+  var param = req.params.date_string;
+  if(param !== undefined){
+    date = parseInt(req.params.date_string*1);
+    //check for unixTimeStamp
+    if(isNaN(date)){
+      date = new Date(req.params.date_string);
+    }else{
+      date = new Date(date);
+    }
+  }else{
+    date = new Date();
+  }
+  
+  var response = date.toUTCString() === "Invalid Date" ? {"error" : "Invalid Date"} : {"unix": date.getTime(), "utc": date.toUTCString()}
+  console.log(response);
+  res.send(response);
+})
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
